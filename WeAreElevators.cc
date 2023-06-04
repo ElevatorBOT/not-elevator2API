5f182e68a6ef1c4db222a505f0857ad16efba001
ElevatorBOT <135512525+ElevatorBOT@users.noreply.github.com>
2023-06-04 14:19:36 +0300

    Create WeAreElevators.cc

---
+++ WeAreElevators.cc
@@ -0,0 +1,45 @@
+using WeAreDevs;
+
+  if WeAreDevs and ElevatorPlugin.net then
+      WeAreDevs.Kexts.com.System.boot
+  end
+  
+  function using(tag)
+  
+  using and tag==["dll"]
+  
+ using dll.com.windows; do [dll.mainloop]
+ 
+              if not WeAreDevs then
+                wrd2.com/download-api
+             end
+             
+             define com.WeAreDevs:Remove(a)
+             
+             a:Remove
+             
+                  newpromise(wearedevs.dll)
+                  
+                          using prompt.exe;
+                          
+                          
+                              prompt:LaunchElevatorBrowser(website: wearedevs.system/wrd2api)
+                              
+                              
+                              return promise(Prompt.NewShell.CurrrentCommand)
+                              
+                              shell.import(wearedevs.dll), elevator.exe
+                              
+                              
+               if microsoft.pc then
+               return
+               end
+               
+               if apple.pc or os then
+               return apple.com
+               end
+               
+               if wine.LinuxApp then
+               end
+               
+ load()
